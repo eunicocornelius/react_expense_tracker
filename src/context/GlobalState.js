d1a0717be18fbb1a1b4d4654a3943c2ce48ad2be
Nico <eunico.cornelius@gmail.com>
2020-10-13 02:59:16 +0700

    init commit

---
+++ src/context/GlobalState.js
@@ -0,0 +1,42 @@
+// This is set to global since this is a small app. If the app is larger in scale, every object would have their own state(eg. shop, profile, etc)
+
+import React, {  createContext, useReducer  } from 'react';
+
+import AppReducer from './AppReducer';
+
+
+// Initial state
+const initialState = {
+    transactions: []
+}
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    // Actions
+    function deleteTransaction(id){
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
+    function addTransaction(transaction){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
+
+    return(<GlobalContext.Provider value={{
+        transactions:state.transactions,
+        deleteTransaction,
+        addTransaction
+        }}>
+        {children}
+    </GlobalContext.Provider>);
+}
