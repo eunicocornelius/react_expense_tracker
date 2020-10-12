bf49fac98d20931bc1d6bd4c88c064439597a9ef
Nico <eunico.cornelius@gmail.com>
2020-10-13 03:03:40 +0700

    Updates

---
+++ precache-manifest.fc19201ec8ed41c7d998cabcec292660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba7f743329487f159a5c49fe81a4fd5",
+    "url": "/react_expense_tracker/index.html"
+  },
+  {
+    "revision": "5e16393a8a85708f8153",
+    "url": "/react_expense_tracker/static/css/main.009d6504.chunk.css"
+  },
+  {
+    "revision": "6302c5f6fc695e4f4736",
+    "url": "/react_expense_tracker/static/js/2.4eefddfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_expense_tracker/static/js/2.4eefddfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e16393a8a85708f8153",
+    "url": "/react_expense_tracker/static/js/main.c5cfb7b2.chunk.js"
+  },
+  {
+    "revision": "39d7adae917b83d97a6d",
+    "url": "/react_expense_tracker/static/js/runtime-main.e3164754.js"
+  }
+]);
